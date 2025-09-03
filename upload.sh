ed401e01f9f3df16976df590d4ba24477875195d
Eberhard Beilharz <eb1@sil.org>
2025-09-03 23:05:55 +0200

    Add installation function

--- upload.sh
+++ upload.sh
@@ -3,7 +3,15 @@ SYNC_PATH="$(awk -F "=" '/syncpath/ {print $2}' config.ini | tr -d ' ')"
 WORK_PATH="$(awk -F "=" '/workpath/ {print $2}' config.ini | tr -d ' ')"
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 
-[ -d "${WORK_PATH}" ] || mkdir -p "${WORK_PATH}"
+if [[ ! -d "${SCRIPT_DIR}/venv" ]]; then
+  python3 -m venv "${SCRIPT_DIR}/venv"
+  . venv/bin/activate
+  pip install stravalib
+fi
+
+. venv/bin/activate
+
+[[ -d "${WORK_PATH}" ]] || mkdir -p "${WORK_PATH}"
 
 cp "${SYNC_PATH}/Gadgetbridge.zip" "${WORK_PATH}/Gadgetbridge.zip"
 
